46d23074b3475a06cd2e67e0748c2a6b07318491
Dot8Pixels <napatsriz@gmail.com>
2025-12-07 23:40:23 +0700

    Fix remote debugging port and add division functionality

--- projects/project_a/src/main.py
+++ projects/project_a/src/main.py
@@ -1,11 +1,15 @@
 import json
 
+from libs import divide
 from modules.multiply import multiply
 
 
 def main():
-    result = multiply(4, 5)
-    print(f"The product of 4 and 5 is: {result}")
+    multiply_result = multiply(4, 5)
+    print(f"The product of 4 and 5 is: {multiply_result}")
+
+    division_result = divide.divide(20, 4)
+    print(f"The division of 20 by 4 is: {division_result}")
 
     with open("config/config.json", "r") as file:
         config = json.load(file)
